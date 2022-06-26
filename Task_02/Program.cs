8c911be1be9b2173964943fe8e53e716944bb657
tunan1994 <tunan@mail.ru>
2022-06-26 20:26:24 +0300

    Добавил_Task2

---
+++ Task_02/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, 
+// какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 > num2)
+{
+    Console.WriteLine($"Число {num1} - большее; а число {num2} - меньшее");
+}
+else
+{
+    Console.WriteLine($"Число {num2} - большее; а число {num1} - меньшее");
+}
--- Task_03/Program.cs
+++ Task_03/Program.cs
@@ -6,31 +6,31 @@
 Console.WriteLine("Введите целое число дня недели: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
-if (num==1)
+if (num == 1)
 {
     Console.WriteLine("Понедельник");
 }
-else if (num==2)
+else if (num == 2)
 {
     Console.WriteLine("Вторник");
 }
-else if (num==3)
+else if (num == 3)
 {
     Console.WriteLine("Среда");
 }
-else if (num==4)
+else if (num == 4)
 {
     Console.WriteLine("Четверг");
 }
-else if (num==5)
+else if (num == 5)
 {
     Console.WriteLine("Пятница");
 }
-else if (num==6)
+else if (num == 6)
 {
     Console.WriteLine("Суббота");
 }
-else if (num==7)
+else if (num == 7)
 {
     Console.WriteLine("Воскресенье");
 }
