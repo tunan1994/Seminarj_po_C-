239629472dfd5812cc4573b2b8a0fef47fd41f2c
tunan1994 <tunan@mail.ru>
2022-06-26 19:13:49 +0300

    add_Task0,1

--- Task_00/Program.cs
+++ Task_00/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход
+// принимает число и выдаёт его квадрат (число
+// умноженное на само себя).
+// Например:
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
+
+Console.Write("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int square = num * num;
+Console.Write($"Квадрат числа {num} = {square}");
---
+++ Task_01/Program.cs
@@ -0,0 +1,18 @@
+﻿// 1. Напишите программу, которая на вход принимает два
+// числа и проверяет, является ли первое число квадратом
+// второго.
+
+Console.WriteLine("Введите первое целое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num2*num2==num1)
+{
+    Console.WriteLine($"Число {num1} являеется квадратом числа {num2}");
+}
+else
+{
+    Console.WriteLine($"Число {num1} не являеется квадратом числа {num2}");
+}
+// int square = num * num;
+// Console.Write($"Квадрат числа {num} = {square}");
