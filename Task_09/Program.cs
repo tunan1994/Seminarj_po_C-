e370736959c5f170c2d7a2e64502569cb813be0c
tunan1994 <tunan@mail.ru>
2022-06-28 20:50:08 +0300

    Добавил_Task09

--- Task_09/Program.cs
+++ Task_09/Program.cs
@@ -5,11 +5,40 @@
 // 12-> 2
 // 85 -> 8
 
+
 int rndNum = new Random().Next(10, 100);
 Console.WriteLine($"Случайное число из отрезка 10 - 99 = > {rndNum}");
 
-int firstDigit = rndNum / 10;
-int secontDigit = rndNum % 10;
-if (firstDigit > secontDigit) Console.WriteLine($"Наибольшая цифра числа {rndNum} => {firstDigit}");
-if (firstDigit < secontDigit) Console.WriteLine($"Наибольшая цифра числа {rndNum} => {secontDigit}");
-if (firstDigit == secontDigit) Console.WriteLine($"Цифры числа {rndNum} => равны");
+// int firstDigit = rndNum / 10;
+// int secontDigit = rndNum % 10;
+
+// int maxDigit = firstDigit > secontDigit ? firstDigit : secontDigit;
+// Console.WriteLine($"Наибольшая цифра числа {rndNum} => {maxDigit}");
+// if (secontDigit == firstDigit)
+//     Console.WriteLine($"Цифры числа {rndNum} равны");
+
+
+int MaxDigit(int num)
+{
+    int firstDigit = num / 10;
+    int secontDigit = num % 10;
+    if (firstDigit == secontDigit) return 0;
+    return firstDigit > secontDigit ? firstDigit : secontDigit;
+}
+
+int maxDigit = MaxDigit(rndNum);
+string result = maxDigit > 0 ? maxDigit.ToString() : "числа равны";
+Console.WriteLine($"Наибольшая цифра числа {rndNum} => {result}");
+
+
+// int maxDigit = firstDigit;
+// if (secontDigit > maxDigit) maxDigit = secontDigit;
+// else if (secontDigit == maxDigit)  maxDigit = 0;
+
+// if(maxDigit > 0) Console.WriteLine($"Наибольшая цифра числа {rndNum} => {maxDigit}");
+// else Console.WriteLine($"Цифры числа {rndNum} равны");
+
+
+// if (firstDigit > secontDigit) Console.WriteLine($"Наибольшая цифра числа {rndNum} => {firstDigit}");
+// if (firstDigit < secontDigit) Console.WriteLine($"Наибольшая цифра числа {rndNum} => {secontDigit}");
+// if (firstDigit == secontDigit) Console.WriteLine($"Цифры числа {rndNum} => равны");
