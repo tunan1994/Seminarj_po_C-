c99885ff9c4bda7f83ef091f893d1da87a542740
tunan1994 <tunan@mail.ru>
2022-07-05 23:45:36 +0300

    add T21_22

---
+++ Task_21/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 21: Напишите программу, которая
+// принимает на вход координаты двух точек и
+// находит расстояние между ними в 2D
+// пространстве.
+// A (3,6); B (2,1) -> 5,09
+// A (7,-5); B (1,-1) -> 7,21
+
+Console.Clear();
+Console.WriteLine("Введите координаты точки A1");
+Console.Write("x1: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("y1: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты точки A2");
+Console.Write("x2: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("y2: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+double A12 = Math.Sqrt(((x2-x1)*(x2-x1)) + ((y2-y1)*(y2-y1)));
+Console.WriteLine($"Расстояние между точками А1 и A2 = {Math.Round(A12, 2)}");
---
+++ Task_22/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 22: Напишите программу, которая
+// принимает на вход число (N) и выдаёт таблицу
+// квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+while ( i <= num)
+{
+Console.WriteLine($"{i} | {i*i}");
+i++;
+}
