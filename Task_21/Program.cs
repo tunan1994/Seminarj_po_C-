c3dd204c9b28d0d18a7e386486bac62e9f03ce83
tunan1994 <tunan@mail.ru>
2022-07-06 00:34:59 +0300

    add T19,21,23

--- Task_21/Program.cs
+++ Task_21/Program.cs
@@ -1,9 +1,8 @@
-﻿// Задача 21: Напишите программу, которая
-// принимает на вход координаты двух точек и
-// находит расстояние между ними в 2D
-// пространстве.
-// A (3,6); B (2,1) -> 5,09
-// A (7,-5); B (1,-1) -> 7,21
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек 
+// и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
 
 Console.Clear();
 Console.WriteLine("Введите координаты точки A1");
@@ -11,11 +10,16 @@ Console.Write("x1: ");
 int x1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("y1: ");
 int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("z1: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+
 Console.WriteLine("Введите координаты точки A2");
 Console.Write("x2: ");
 int x2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("y2: ");
 int y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("z2 ");
+int z2 = Convert.ToInt32(Console.ReadLine());
 
-double A12 = Math.Sqrt(((x2-x1)*(x2-x1)) + ((y2-y1)*(y2-y1)));
-Console.WriteLine($"Расстояние между точками А1 и A2 = {Math.Round(A12, 2)}");
+double A12 = Math.Sqrt(((x2-x1)*(x2-x1)) + ((y2-y1)*(y2-y1)) + ((z2-z1)*(z2-z1)));
+Console.WriteLine($"Расстояние между точками А1 и A2 в 3D пространстве = {Math.Round(A12, 2)}");
---
+++ Task_23/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= num; i++)
+{
+Console.WriteLine($" |  {i} | {i*i*i, 5} |");
+}
